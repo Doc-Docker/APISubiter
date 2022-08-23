185feeec6458796ce63d5c2281c16e7a236df157
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-08-22 22:13:29 -0300

    demo-project

---
+++ APISubiterBackend/src/test/java/com/subiter/backend/APISubiterBackend/ApiSubiterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subiter.backend.APISubiterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSubiterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
