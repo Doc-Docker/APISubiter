8d0e4647baf57f880527c10c75f815ca21eecf48
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2022-09-04 18:49:11 -0300

    Criação da requisição GET dos serviços

--- apisubiterfrontend/src/views/ListagemServicos.vue
+++ apisubiterfrontend/src/views/ListagemServicos.vue
@@ -2,9 +2,7 @@
   <h1>Listagem</h1>
   <div class="container text-center">
     <div class="row align-items-start">
-      <div class="col">One of three columns</div>
-      <div class="col">One of three columns</div>
-      <div class="col">One of three columns</div>
+      <div class="col">{{servicos}}</div>
     </div>
     <div class="row align-items-center">
       <div class="col">One of three columns</div>
@@ -18,3 +16,25 @@
     </div>
   </div>
 </template>
+
+<script lang='ts'>
+import { defineComponent, onMounted, ref } from 'vue'
+import http from '../http/index'
+
+export default defineComponent({
+  name: "ListagemServicos",
+  setup(){
+    const servicos = ref([])
+
+    const fetchServicos =() => 
+      http
+        .get("servicos")
+        .then((response) => (servicos.value = response.data));
+
+    onMounted(fetchServicos);
+
+    return {servicos};
+
+  }
+})
+</script>
