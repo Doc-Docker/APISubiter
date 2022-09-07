7dcbf0173d615ba7d4a9b4b2e110fdc2d8722e88
José Maria reis dos Santos <80988756+Jose-dos-Santos@users.noreply.github.com>
2022-09-07 19:23:32 -0300

    Tarefa Equipamento_Serie

--- src/main/java/com/subiter/backend/APISubiterBackend/rest/controllers/EquipamentoSerieController.java
+++ src/main/java/com/subiter/backend/APISubiterBackend/rest/controllers/EquipamentoSerieController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.subiter.backend.APISubiterBackend.model.entity.Equipamento;
 import com.subiter.backend.APISubiterBackend.model.entity.EquipamentoSerie;
 import com.subiter.backend.APISubiterBackend.service.EquipamentoSerieService;
 
