d7ba8b78d50bb588cea4b69acbbcde9e7b67727e
Antonio Zago <antoniozagodev@gmail.com>
2022-09-09 19:52:02 -0300

    Projeto novo

---
+++ APISubiterFrontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <CabecalhoComponent/>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CabecalhoComponent from './components/CabecalhoComponent.vue'
+
+export default {
+  components:{
+    CabecalhoComponent
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ APISubiterFrontend/src/views/CadastroServicoManutencaoView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="cadastroServicos">
+    <form @submit.prevent="salvar">
+      <div class="mb-3 mt-3">
+        <div class="row">
+          <div class="col-md-6">
+            <h3>Serviço de manutenção</h3>
+          </div>  
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <div class="row">
+          <div class="col-md-6">
+            <label for="exampleFormControlInput1" class="form-label"
+              >Empresa</label
+            >
+            <input
+              type="text"
+              class="form-control"
+              v-model="servico.empresa"
+            />
+          </div>
+        </div>
+      </div>
+
+
+      <div class="mb-5">
+        <div class="row">
+          <div class="col-md-6">
+            <label for="exampleFormControlTextarea1" class="form-label"
+              >Descrição</label
+            >
+            <textarea
+              class="form-control"
+              v-model="servico.descricao"
+              rows="3"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+      <button>Salvar</button>
+    </form>
+
+    
+  </div>
+</template>
+
+<style scoped>
+.cadastroServicos {
+  padding: 1.25rem;
+}
+</style>
+
+<script>
+import Servico from '../services/servicos'
+
+export default{
+  name: "CadastroServicosManutencaoView",
+
+  data(){
+    return{
+        servico:{
+            tipo_servico:'Manutencao',
+            descricao:'',
+            empresa:''
+        }
+    }
+  },
+  methods:{
+    salvar(){
+      Servico.salvar(this.servico).then(resposta => {
+        alert('Salvo com sucesso' + resposta)
+      })
+      
+    }
+  }
+};
+</script>
