2d272eb824ef2e4bc2eb0b807c30b56a3f63e763
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-09-13 19:52:09 -0300

    Criação de relacionamento entre Usuario e Empresa

--- APISubiterBackend/src/main/java/com/subiter/backend/APISubiterBackend/model/entity/Usuario.java
+++ APISubiterBackend/src/main/java/com/subiter/backend/APISubiterBackend/model/entity/Usuario.java
@@ -3,19 +3,23 @@ package com.subiter.backend.APISubiterBackend.model.entity;
 import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
-@Entity
+@Entity(name = "USUARIO")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Usuario {
+public class Usuario implements Serializable{
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_usuario")
     private Integer id;
 
-    @Column(nullable = false, length = 150)
+    @Column(name = "nome_usuario",nullable = false, length = 150)
     @NotEmpty(message = "O Campo nome é obrigatório")
     private String name;
 
@@ -23,6 +27,8 @@ public class Usuario {
 
     // private String senha_usuario;
 
-    // private Integer empresa
+    @JoinColumn(name="codigo_empresa")
+    @ManyToOne()
+    private Empresa empresa;
     
 }
