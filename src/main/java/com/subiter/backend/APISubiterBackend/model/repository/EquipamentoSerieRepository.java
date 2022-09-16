2523b9122759c0399682c10fa41d02521c7e217f
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-09-16 20:50:51 -0300

    Retirando arquivos do back na pasta central

--- src/main/java/com/subiter/backend/APISubiterBackend/model/repository/EquipamentoSerieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.subiter.backend.APISubiterBackend.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.subiter.backend.APISubiterBackend.model.entity.EquipamentoSerie;
-
-public interface EquipamentoSerieRepository extends JpaRepository<EquipamentoSerie, Integer>{
-
-}
