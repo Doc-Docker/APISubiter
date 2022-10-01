9e756cae527d2192d038b7c29d4ae7dfc2701e85
camilaffpacheco <camilapacheco.contato@gmail.com>
2022-10-01 15:12:07 -0300

    importando biblioteca primevue

--- APISubiterFrontend/src/main.js
+++ APISubiterFrontend/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/primevue.min.css';
+import'primeicons/primeicons.css';
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  PrimeVue,
   render: h => h(App)
 }).$mount('#app')
