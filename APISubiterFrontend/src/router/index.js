1d8c7834855772061d9d6d34269a7d2e87f64f4b
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-10-22 12:09:24 -0300

    Children in screens

--- APISubiterFrontend/src/router/index.js
+++ APISubiterFrontend/src/router/index.js
@@ -1,101 +1,74 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-// Listagens
-import ListagemServicosView from '../views/ListagemServicosView.vue'
-import ListagemEquipamentoView from '../views/ListagemEquipamentoView.vue'
-
-import ListagemChamadoSuporteView from '../views/ListagemChamadoSuporteView.vue'
-
-import ListagemEmpresaView from '../views/ListagemEmpresaView.vue'
-import ListagemUsuarios from '../views/ListagemUsuariosView.vue'
-import ListagemChamadoCliente from '../views/ListagemChamadoClienteView.vue'
-
-// Cadastros
-import CadastroServicoManutencao from '../views/CadastroServicoManutencaoView.vue'
-import CadastroServicoInstalacao from '../views/CadastroServicoInstalacaoView.vue'
-import CadastroEquipamento from '../views/CadastroEquipamentoView.vue'
-import CadastroEmpresa from '../views/CadastroEmpresaView.vue'
-import CadastroUsuario from '../views/CadastroUsuarioView.vue'
-
-
-import CadastroChamado from '../views/CadastroChamadoView.vue'
+import AppView from '../views/AppView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/servicos',
-    name: 'listagemServicos',
-    component: ListagemServicosView
-  },
-  {
-    path: '/equipamentos',
-    name: 'listagemEquipamentos',
-    component: ListagemEquipamentoView
-  },
-  {
-    path: '/SuporteChamados',
-    name: 'suporteChamados',
-    component: ListagemChamadoSuporteView
-  },
-  {
-    path: '/empresas',
-    name: 'listagemEmpresas',
-    component: ListagemEmpresaView
-  },
-  {
-    path: '/ChamadoCliente',
-    name: 'ChamadoCliente',
-    component: ListagemChamadoCliente
-  },
-  {
-    path: '/cadastroServicoManutencao',
-    name: 'cadastroServicoManutencao',
-    component: CadastroServicoManutencao
-  },
-  {
-    path: '/cadastroServicoInstalacao',
-    name: 'cadastroServicoInstalacao',
-    component: CadastroServicoInstalacao
-  },
-  {
-    path: '/cadastroEquipamentos',
-    name: 'cadastroEquipamentos',
-    component: CadastroEquipamento
-  },
-  {
+    name: 'app',
+    component: AppView,
+    children: [
+      {
+        path: '/servicos',
+        component: () =>('../views/ListagemServicosView.vue')
+      },
+      {
+        path: '/equipamentos',
+        component: () =>('../views/ListagemEquipamentoView.vue')
+      },
+      {
+        path: '/SuporteChamados',
+        component: () =>('../views/ListagemChamadoSuporteView.vue')
+      },
+      {
+        path: '/empresas',
+        component: () =>('../views/ListagemEmpresaView.vue')
+      },
+      {
+        path: '/ChamadoCliente',
+        component: () =>('../views/ListagemChamadoClienteView.vue')
+      },
+      {
+        path: '/cadastroServicoManutencao',
+        component: () =>('../views/CadastroServicoManutencaoView.vue')
+      },
+      {
+        path: '/cadastroServicoInstalacao',
+        component: () =>('../views/CadastroServicoInstalacaoView.vue')
+      },
+      {
+        path: '/cadastroEquipamentos',
+        component: () =>('../views/CadastroEquipamentoView.vue')
+      },
+      {
 
-    path: '/cadastroEmpresas',
-    name: 'cadastroEmpresas',
-    component: CadastroEmpresa
-  },
-  {
+        path: '/cadastroEmpresas',
+        component: () =>('../views/CadastroEmpresaView.vue')
+      },
+      {
 
-    path: '/cadastroUsuario',
-    name: 'cadastroUsuario',
-    component: CadastroUsuario
-  },
-  {
-    path: '/usuarios',
-    name: 'listagemUsuarios',
-    component: ListagemUsuarios
+        path: '/cadastroUsuario',
+        component: () =>('../views/CadastroUsuarioView.vue')
+      },
+      {
+        path: '/usuarios',
+        component: () =>('../views/ListagemUsuariosView.vue')
 
-  },
-  {
-    path: '/cadastroChamado',
-    name: 'cadastroChamado',
-    component: CadastroChamado
+      },
+      {
+        path: '/cadastroChamado',
+        component: () =>('../views/CadastroChamadoView.vue')
+      }
+    ]
   }
+
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
