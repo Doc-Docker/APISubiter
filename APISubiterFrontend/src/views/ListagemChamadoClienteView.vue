aaf674f0d0b9c41eeabd6ae0f6a44de3ca501bd8
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-11-06 00:40:23 -0300

    Listagem Cliente

--- APISubiterFrontend/src/views/ListagemChamadoClienteView.vue
+++ APISubiterFrontend/src/views/ListagemChamadoClienteView.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="table">
     <div class="mb-3 mt-3">
       <div class="row">
         <div class="col-md-6">
@@ -8,15 +9,15 @@
       <table class="table table-striped table-bordered" style="text-align: center">
         <thead>
           <tr>
-            <th scope="col">Nome Usuário</th>
-            <th scope="col">Tipo de Chamado</th>
-            <th scope="col">Criticidade</th>
-            <th scope="col">Data de Chamado</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Situação Chamado</th>
-            <th scope="col">Solução</th>
-            <th scope="col">Data Encerramento</th>
-            <th scope="col">Ações</th>
+            <th scope="col-md-6">Nome Usuário</th>
+            <th scope="col-md-6">Tipo de Chamado</th>
+            <th scope="col-md-6">Criticidade</th>
+            <th scope="col-md-6">Data de Chamado</th>
+            <th scope="col-md-6">Descrição</th>
+            <th scope="col-md-6">Situação Chamado</th>
+            <th scope="col-md-6">Solução</th>
+            <th scope="col-md-6">Data Encerramento</th>
+            <th scope="col-md-6">Ações</th>
           </tr>
         </thead>
         <tbody>
@@ -30,149 +31,164 @@
             <td>{{ chamado_cliente.solucaoChamado }}</td>
             <td>{{ chamado_cliente.encerramentoChamado }}</td>
             <td>
-              <button class="btn btn-danger" @click="deletar(chamado_cliente.id)" style="margin-bottom: 5px" hidden >
+              <button class="btn btn-danger" @click="deletar(chamado_cliente.id)" style="margin-bottom: 5px" hidden>
                 Deletar
               </button>
-              <button class="btn btn-primary" @click="editar(chamado_cliente)">Editar</button>
+              <b-button v-b-modal.modal-1>Editar Chamado</b-button>
+              
             </td>
           </tr>
         </tbody>
       </table>
-    <div>
-    </div>
-    <form @submit.prevent="salvar">
-        <div class="mb-3 mt-3">
-          <div class="row">
-            <div class="col-md-6">
-              <h3>Chamado Cliente</h3>
-            </div>  
-          </div>
-        </div>
-  
-        <div class="mb-3">
-          <div class="row">
-            <div class="col-md-6">
-              <label for="exampleFormControlInput1" class="form-label"
-                >Nome do Usuário</label
-              >
-              <input
-                type="text"
-                class="form-control"
-                v-model="chamado_cliente.usuarioChamado.name"
-                disabled
-              />
-            </div>
-          </div>
-        </div>
+      <div>
+      </div>
 
-        <div class="mb-3">
-          <div class="row">
-            <div class="col-md-6">
-            <label for="exampleFormControlInput2" class="form-label">Criticidade do chamado</label>
-            <br>
-            <select class="form-control" v-model="chamado_cliente.criticidadeChamado">
-              <option id="B">Baixo</option>
-              <option id="M">Médio</option>
-              <option id="A">Alto</option>
-            </select>
-            </div>
-          </div>
-        </div>
-
-        <div class="mb-3">
-          <div class="row">
-            <div class="col-md-6">
-              <label for="exampleFormControlInput1" class="form-label"
-                >Descrição</label
-              >
-              <input
-                type="text"
-                class="form-control"
-                v-model="chamado_cliente.descricaoChamado"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div class="mb-3">
-          <div class="row">
-            <div class="col-md-6">
-              <label for="exampleFormControlInput1" class="form-label"
-                >Situação</label
-              >
-              <input
-                type="text"
-                class="form-control"
-                v-model="chamado_cliente.situacaoChamado"
-                disabled
-              />
-            </div>
-          </div>
-        </div>
+      <div>
         
-        <button class="btn btn-success">Salvar</button>
-      </form>
-  
+        <b-modal id="modal-1" title="BootstrapVue">
+          <button class="btn btn-primary" @click="editar(chamado_cliente)">Editar</button>
+          <form @submit.prevent="salvar">
+            <div class="mb-3 mt-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <h3>Chamado Cliente</h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="exampleFormControlInput1" class="form-label">Nome do Usuário</label>
+                  <input type="text" class="form-control" v-model="chamado_cliente.usuarioChamado.name" disabled />
+                </div>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="exampleFormControlInput2" class="form-label">Criticidade do chamado</label>
+                  <br>
+                  <select class="form-control" v-model="chamado_cliente.criticidadeChamado">
+                    <option id="B">Baixo</option>
+                    <option id="M">Médio</option>
+                    <option id="A">Alto</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="exampleFormControlInput1" class="form-label">Descrição</label>
+                  <input type="text" class="form-control" v-model="chamado_cliente.descricaoChamado" />
+                </div>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="exampleFormControlInput1" class="form-label">Situação</label>
+                  <input type="text" class="form-control" v-model="chamado_cliente.situacaoChamado" disabled />
+                </div>
+              </div>
+            </div>
+
+            <button class="btn btn-success">Salvar</button>
+          </form>
+        </b-modal>
+      </div>
     </div>
-  </template>
+  </div>
+
+</template>
   
-  <script>
-  import Chamado_Cliente from "../services/chamado_cliente";
-  
-  export default {
-    name: "ChamadoClienteView",
-  
-    data() {
-      return {
-        chamado_clientes: [],
-        chamado_cliente: {
-          id: "",
-          usuarioChamado: "",
-          tipoChamado: "",
-          criticidadeChamado: "",
-          dataChamado: "",
-          descricaoChamado: "",
-          situacaoChamado: "",
-          encerramentoChamado: "",
-        },
-      };
+<script>
+import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+import Chamado_Cliente from "../services/chamado_cliente";
+
+export default {
+  name: "ChamadoClienteView",
+
+  data() {
+    return {
+      chamado_clientes: [],
+      chamado_cliente: {
+        id: "",
+        usuarioChamado: "",
+        tipoChamado: "",
+        criticidadeChamado: "",
+        dataChamado: "",
+        descricaoChamado: "",
+        situacaoChamado: "",
+        encerramentoChamado: "",
+      },
+    };
+  },
+  mounted() {
+    this.listar();
+  },
+  methods: {
+    listar() {
+      Chamado_Cliente.listar().then((resposta) => {
+        const resp = resposta.data;
+        const result = resp.filter(resp => resp.usuarioChamado.name === "usuario.cliente");
+        this.chamado_clientes = result;
+      });
     },
-    mounted() {
-      this.listar();
+    deletar(id) {
+      Chamado_Cliente.deletar(id).then(() => {
+        this.listar();
+        alert("Deletado com Sucesso");
+      });
     },
-    methods: {
-      listar() {
-        Chamado_Cliente.listar().then((resposta) => {
-          const resp = resposta.data;
-          const result = resp.filter(resp => resp.usuarioChamado.name === "usuario.cliente");
-          this.chamado_clientes = result;
-        });
-      },
-      deletar(id) {
-        Chamado_Cliente.deletar(id).then(() => {
-          this.listar();
-          alert("Deletado com Sucesso");
-        });
-      },
-      editar(chamado_cliente) {
-        this.chamado_cliente = chamado_cliente;
-      },
-      salvar(){
-        console.log(this.chamado_cliente)
-        Chamado_Cliente.atualizar(this.chamado_cliente).then(()=>{
-          alert('Atualizado com sucesso!');
-          this.limparFormularios();
-          this.listar();
-        })
-      },
-      limparFormularios() {
-        this.chamado_cliente.usuarioChamado = "";
-        this.chamado_cliente.criticidadeChamado = "";
-        this.chamado_cliente.descricaoChamado = "";
-        this.chamado_cliente.situacaoChamado = "";
-      }
+    editar(chamado_cliente) {
+      this.chamado_cliente = chamado_cliente;
     },
-  };
-  </script>
+    salvar() {
+      console.log(this.chamado_cliente)
+      Chamado_Cliente.atualizar(this.chamado_cliente).then(() => {
+        alert('Atualizado com sucesso!');
+        this.limparFormularios();
+        this.listar();
+      })
+    },
+    limparFormularios() {
+      this.chamado_cliente.usuarioChamado = "";
+      this.chamado_cliente.criticidadeChamado = "";
+      this.chamado_cliente.descricaoChamado = "";
+      this.chamado_cliente.situacaoChamado = "";
+    }
+  },
+};
+</script>
   
-  
+<style scoped>
+table {
+  margin-left: 50px;
+  padding: 1.30rem;
+  width: 90%;
+  border-collapse: collapse;
+  border: 1px solid black;
+  text-align: center;
+  table-layout: fixed;
+
+}
+
+td,
+th {
+  border: 1px solid black;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
